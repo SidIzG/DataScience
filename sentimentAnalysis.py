eb9414f1935bec6e7fbb26811ba15e6294f3e01c
SidIzG <33462891+SidIzG@users.noreply.github.com>
2020-01-03 19:03:56 -0500

    Sentiment Analysis Arnold Schwarzenegger Twitter
    This is a sentiment analysis of Arnold Schwarzenegger using Twitter's API. I played with my code to allow me to see how other topics are trending right now.

---
+++ sentimentAnalysis.py
@@ -0,0 +1,6 @@
+import os
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.ensemble import RandomForestClassifier
+from KaggleWord2VecUtility import KaggleWord2VecUtility
+
+
